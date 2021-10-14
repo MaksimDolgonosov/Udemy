c6a8b93d160c59589750b08f7abd1f0a991b6ecd
Maksim Dauhanosav <max_air@bk.ru>
2021-10-14 15:25:10 +0300

    Final version 12 unit practice

--- Раздел 12. Начинаем создавать приложение/script.js
+++ Раздел 12. Начинаем создавать приложение/script.js
@@ -1,8 +1,19 @@
-let numberOfFlms= prompt("Сколько фильмов вы уже посмотрели?");
-let PersonalMovieDB = {
+const numberOfFlms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const PersonalMovieDB = {
     count: numberOfFlms,
     movies: {},
     actors: {},
     genres: [],
-    privat:false
+    privat: false
 };
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов", ""),
+    d = prompt("На сколько оцените его?", "");
+
+PersonalMovieDB.movies[a] = b;
+PersonalMovieDB.movies[c] = d;
+
+console.log(PersonalMovieDB);
