d8fff3c54e66579579471fda2c6c4bebdd22020f
Maksim Dauhanosav <max_air@bk.ru>
2021-10-15 12:40:41 +0300

    Раздел 15 Практика. Условия и циклы

--- Раздел 12. Начинаем создавать приложение/script.js
+++ Раздел 12. Начинаем создавать приложение/script.js
@@ -9,12 +9,33 @@ const PersonalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов", ""),
-    d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", "");
+    const b = prompt("На сколько оцените его?", "");
 
-PersonalMovieDB.movies[a] = b;
-PersonalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        PersonalMovieDB.movies[a] = b;
+        console.log("done");
+
+    } else {
+        console.log("error");
+        i--;
+    }
+
+
+
+}
+
+if (PersonalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (PersonalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произола ошибка")
+}
+
+
 
 console.log(PersonalMovieDB);
