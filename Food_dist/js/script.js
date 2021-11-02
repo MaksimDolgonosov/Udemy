463e475f8d05c247fb34ea7504a4c980e63fd03c
Maksim Dauhanosav <max_air@bk.ru>
2021-11-02 14:46:24 +0300

    Раздел 38. Добавили табы.

---
+++ Food_dist/js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded",( )=>{
+let tabs = document.querySelectorAll(".tabheader__item");
+let tabsParent=document.querySelector(".tabheader__items");
+let tabsContent=document.querySelectorAll(".tabcontent");
+
+function hideTabContent(){
+    tabsContent.forEach(item=>{
+    item.style.display="none";
+    tabs.forEach(e=>{
+        e.classList.remove("tabheader__item_active");
+    });
+});
+}
+
+function showTabContent(i=0){
+tabs[i].classList.add("tabheader__item_active");
+tabsContent[i].style.display="block";
+tabsContent[i].classList.add("fade");
+}
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener("click",(event)=>{
+    if (event.target && event.target.classList.contains("tabheader__item")){
+       tabs.forEach((item,i) =>{
+          if (item==event.target){
+             hideTabContent();
+            showTabContent(i);
+            }
+        });
+    }
+
+});
+    
+
+});
+
+
+
