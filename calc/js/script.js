62952eb18de0087f96c8dbb1aadbfc7be82dc610
max_air <max_air@bk.ru>
2021-11-15 15:23:44 +0300

    Раздел 52.AJAX сделано

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,9 +1,23 @@
 "use stcrict";
 
-const inputRUB = document.querySelector("#rub");
-const inputUSD = document.querySelector("#usd");
+let inputRUB = document.querySelector("#rub");
+let inputUSD = document.querySelector("#usd");
 
 
 inputRUB.addEventListener("input", () => {
-    e.value
-})
+    let request = new XMLHttpRequest();
+    request.open("GET", "js/current.json");
+    request.setRequestHeader('Content-type', 'applicaton/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener("load", () => {
+        if (request.status == 200) {
+            let data = JSON.parse(request.response);
+            console.log(request.response);
+            inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = "Ошибка";
+        }
+    });
+});
+
