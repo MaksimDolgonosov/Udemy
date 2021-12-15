0d4251a11ce15fd50be90d56c8370ac8c78772c0
Malsim Dauhanosav <max_air@bk.ru>
2021-12-15 19:10:51 +0300

    Раздел 78. Jquery

--- jquery/script.js
+++ jquery/script.js
@@ -4,5 +4,11 @@ $(document).ready(function () {
     $(".list-item:first").hover(function () {
         $(this).toggleClass("active");
     });
+
+    $(".list-item:eq(2)").on("click", function () {
+        $(".image:even").fadeToggle("slow");
+    });
+
+
 });
 
